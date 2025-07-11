fe345806fc8d35ed1b06170a5acd1377fb03fc17
Faisal <faisal.salih.hayat768@gmail.com>
2025-07-11 17:41:13 +0500

    Navbar completed 70%

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,42 @@
 import React from 'react'
 import Logo from '../assets/network.png'
+import Down from "../assets/down-arrow.png"
+import Burger from "../assets/hamburger (1).png"
+
+
 
 const Navbar = () => {
 
   return (
     <>
-    <nav className='sm:w-3/4 w-full h-20 bg-amber-200 d-flex justify-self-center'>
+    <nav className='w-full lg:w-[90%] h-20 bg-amber-200 flex justify-self-center justify-between items-center px-6'>
     {/* Logo of the Navbar */}
-    <div class="logo">
-      <img src={Logo} alt="navbar logo" className='w-16 h-16' />
+
+    
+    <div className="logo">
+      <img src={Logo} alt="navbar logo" className='w-13 h-13 lg:w-16 lg:h-16'/>
     </div>
+    {/* Link Section Start here */}
+    <div className="links hidden md:block">
+      <ul className='bg-blue-500 flex gap-x-8'>
+        <li className='flex gap-1  lg:text-base cursor-pointer text-sm'>About us <img src={Down} alt="Dropdown" className='w-6 h-6'/></li>
+        <li className='flex gap-1 lg:text-base cursor-pointer text-sm'>what we do <img src={Down} alt="Dropdown" className='w-6 h-6'/></li>
+        <li className='flex gap-1 lg:text-base cursor-pointer text-sm'>Our Result</li>
+        <li className='flex gap-1 lg:text-base cursor-pointer text-sm'>About us <img src={Down} alt="Dropdown" className='w-6 h-6'/></li>
+        <li className='flex gap-1 lg:text-base cursor-pointer text-sm'>what we do <img src={Down} alt="Dropdown" className='w-6 h-6'/></li>
+
+      </ul>
+    </div>
+
+    {/* Button Section start from here */}
+    <div className="button hidden md:block">
+      <button className='white border-1 border-[#191919] cursor-pointer rounded-3xl p-2'>Try it Free</button>
+    </div>
+
+    <div className='menu block md:hidden cursor-pointer'>
+      <img src={Burger} alt="Menu_image" className='w-8 h-8'/>
+    </div>
+
     </nav>
     </>
   )

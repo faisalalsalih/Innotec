766a07d28a83fc122ac89a4a8c270a88d17aec5d
Faisal <faisal.salih.hayat768@gmail.com>
2025-07-11 18:13:30 +0500

    Navbar Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Logo from '../assets/network.png'
 import Down from "../assets/down-arrow.png"
-import Burger from "../assets/hamburger (1).png"
+import Burger from "../assets/menu.png"
 
 
 
@@ -9,28 +9,28 @@ const Navbar = () => {
 
   return (
     <>
-    <nav className='w-full lg:w-[90%] h-20 bg-amber-200 flex justify-self-center justify-between items-center px-6'>
+    <nav className='w-full lg:w-[90%] h-20 flex justify-self-center justify-between items-center px-6'>
     {/* Logo of the Navbar */}
 
     
     <div className="logo">
-      <img src={Logo} alt="navbar logo" className='w-13 h-13 lg:w-16 lg:h-16'/>
+      <img src={Logo} alt="navbar logo" className='w-13 h-13 lg:w-15 lg:h-15'/>
     </div>
     {/* Link Section Start here */}
     <div className="links hidden md:block">
-      <ul className='bg-blue-500 flex gap-x-8'>
-        <li className='flex gap-1  lg:text-base cursor-pointer text-sm'>About us <img src={Down} alt="Dropdown" className='w-6 h-6'/></li>
-        <li className='flex gap-1 lg:text-base cursor-pointer text-sm'>what we do <img src={Down} alt="Dropdown" className='w-6 h-6'/></li>
-        <li className='flex gap-1 lg:text-base cursor-pointer text-sm'>Our Result</li>
-        <li className='flex gap-1 lg:text-base cursor-pointer text-sm'>About us <img src={Down} alt="Dropdown" className='w-6 h-6'/></li>
-        <li className='flex gap-1 lg:text-base cursor-pointer text-sm'>what we do <img src={Down} alt="Dropdown" className='w-6 h-6'/></li>
+      <ul className='flex gap-x-6'>
+        <li className='flex  lg:text-base cursor-pointer text-sm text-[#fcf1e3]'>About us <img src={Down} alt="Dropdown" className='w-6 h-6'/></li>
+        <li className='flex  lg:text-base cursor-pointer text-sm text-[#fcf1e3]'>what we do <img src={Down} alt="Dropdown" className='w-6 h-6'/></li>
+        <li className='flex  lg:text-base cursor-pointer text-sm text-[#fcf1e3]'>Our Result</li>
+        <li className='flex  lg:text-base cursor-pointer text-sm text-[#fcf1e3]'>About us <img src={Down} alt="Dropdown" className='w-6 h-6'/></li>
+        <li className='flex  lg:text-base cursor-pointer text-sm text-[#fcf1e3]'>what we do <img src={Down} alt="Dropdown" className='w-6 h-6'/></li>
 
       </ul>
     </div>
 
     {/* Button Section start from here */}
     <div className="button hidden md:block">
-      <button className='white border-1 border-[#191919] cursor-pointer rounded-3xl p-2'>Try it Free</button>
+      <button className='white border-1 border-[#191919] cursor-pointer rounded-3xl lg:pl-8 lg:pr-8 lg:pt-2 lg:pb-2 md:pl-5 md:pr-5 md:pt-2 md:pb-2'>Try it Free</button>
     </div>
 
     <div className='menu block md:hidden cursor-pointer'>

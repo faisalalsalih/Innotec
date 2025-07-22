aa3f08a13e4e3245b8155fbd5b67f455e8471dde
Faisal <faisal.salih.hayat768@gmail.com>
2025-07-22 13:16:34 +0500

    Section 2 is about to complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Team from './components/Team'
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Team />
     </>
   )
 }
---
+++ src/components/Team.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Team = () => {
+  return (
+    <>
+      <section id='team' className='w-full md:w-[85%] h-dvh bg-amber-300 justify-self-center p-2 my-22 flex items-center  '>
+
+        <div className="center w-full bg-black h-120">
+            <div className="center_upper"></div>
+            <div className="sticky_1"></div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Team

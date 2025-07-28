67e555435ea3a627e49b7e7d83cbe4fd32b58347
faisalalsalih <faisal.salih.hayat768@gmail.com>
2025-07-28 11:57:04 +0500

    Section Three Started

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Team from './components/Team'
+import Solutions from './components/Solutions'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Team />
+      <Solutions />
     </>
   )
 }
---
+++ src/components/Solutions.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Solutions = () => {
+  return (
+    <>
+    <section id='solutions' className='w-full lg:w-[90%] h-dvh bg-green-600 justify-self-center'>
+        <div className="offer">
+            <div className="offer_1">
+              
+            </div>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Solutions
